6162694613c23ab58bac5cbf7d60676008e3fd07
felipedelatorre <felipede_latorre@hotmail.com>
2019-07-13 19:35:58 -0700

    add CRD. Misiing U

--- index.py
+++ index.py
@@ -14,9 +14,22 @@ def hello_world():
 
 @app.route('/sales')
 def sales():
-    return render_template("sales.html", total=42)
+    bend_data = mongo.db.locations.find_one({"city": "Bend" })
+    redmond_data = mongo.db.locations.find_one({"city": "Redmond" })
+    prineville_data = mongo.db.locations.find_one({"city": "Prineville" })
+    return render_template("sales.html", bend = bend_data, redmond = redmond_data, prineville = prineville_data)
 
 @app.route('/create', methods=['POST'])
 def create():
-    data = mongo.db.locations.insert({"location": "TerrorBone"})
+    data = mongo.db.locations.insert_many([
+    {"city": "Bend", "sales": [13, 5, 7, 8, 0]},
+    {"city": "Redmond", "sales": [1, 6, 15, 3, 4]},
+    {"city": "Prineville", "sales": [9, 12, 11, 14, 10]},
+])
+
     return "Data created"
+
+@app.route('/destroy', methods=['POST'])
+def destroy():
+    data = mongo.db.locations.drop()
+    return "Data destroyed"
