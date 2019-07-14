833dd97789d4a40446ef23944ddfe440d898419a
felipedelatorre <felipede_latorre@hotmail.com>
2019-07-13 20:19:58 -0700

    add final css, configs, and logic

--- index.py
+++ index.py
@@ -1,6 +1,12 @@
+import os
 from settings import NAME
 from flask import Flask, escape, request, render_template, url_for
 from flask_pymongo import PyMongo
+
+MONGO_URL = os.environ.get('MONGODB_URI')
+if not MONGO_URL:
+    MONGO_URL = "mongodb://localhost:27017/salmonCookies";
+
 
 app = Flask(__name__)
 app.config["MONGO_URI"] = "mongodb://localhost:27017/salmonCookies"
@@ -17,6 +23,15 @@ def sales():
     bend_data = mongo.db.locations.find_one({"city": "Bend" })
     redmond_data = mongo.db.locations.find_one({"city": "Redmond" })
     prineville_data = mongo.db.locations.find_one({"city": "Prineville" })
+    if bend_data is None:
+        bend_data = {"city": "Bend", "sales": [0, 0, 0, 0, 0]}
+
+    if redmond_data is None:
+        redmond_data = {"city": "Redmond", "sales": [0, 0, 0, 0, 0]}
+
+    if prineville_data is None:
+        prineville_data = {"city": "Prineville", "sales": [0, 0, 0, 0, 0]}
+
     return render_template("sales.html", bend = bend_data, redmond = redmond_data, prineville = prineville_data)
 
 @app.route('/create', methods=['POST'])
@@ -27,9 +42,9 @@ def create():
     {"city": "Prineville", "sales": [9, 12, 11, 14, 10]},
 ])
 
-    return "Data created"
+    return render_template("home.html", name=NAME)
 
 @app.route('/destroy', methods=['POST'])
 def destroy():
     data = mongo.db.locations.drop()
-    return "Data destroyed"
+    return render_template("home.html", name=NAME)
